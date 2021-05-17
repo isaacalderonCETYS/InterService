59dcfb07c2af6fd5d0bc90fb35d1ffcb5cd565aa
isaacalderonCETYS <isaac.calderon@cetys.edu.mx>
2021-05-16 17:49:31 -0700

    Responsive Header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Sidenav } from 'materialize-css';
 
 @Component({
   selector: 'app-header',
@@ -10,6 +11,10 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.sidenav');
+      var instances = M.Sidenav.init(elems, {});
+    });
   }
 
 }
